a89e48d5aa3d852408f2cc4d0d729ae98835de8c
krukov <dima@futubank.com>
2014-08-06 14:38:52 +0400

    Readme and setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='amocrm_api',
+    version='0.0.1',
+    packages=['amocrm'],
+    url='https://github.com/Krukov/amocrm_api',
+    license='MIT license',
+    author='Dmitry Krukov',
+    author_email='frog-king69@yandex.ru',
+    description='Python (Django like) API for Amocrm',
+    long_description=open('README.rst').read(),
+
+    requires=[
+        'requests (>=2.3)',
+        'responses (>=0.2.2)',
+    ],
+    install_requires=[
+        'requests >=2.3',
+        'responses >=0.2.2',
+    ]
+)
