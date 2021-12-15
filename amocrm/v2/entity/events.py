35c68dd7f12eb7c1b4c4fb3ccfd1ea84211263e7
rolldeep <urumtseval@gmail.com>
2021-12-15 22:04:39 +0300

    Update amocrm/v2/entity/events.py
    Co-authored-by: Krukov D <Glebov.ru@gmail.com>

--- amocrm/v2/entity/events.py
+++ amocrm/v2/entity/events.py
@@ -12,7 +12,7 @@ class EventsInteraction(GenericInteraction):
 class Event(model.Model):
     type = fields._Field("type")
     entity_id = fields._UnEditableField("entity_id")
-    entity_type = fields._Field("note_type")
+    entity_type = fields. _UnEditableField("entity_type")
     created_by = fields._Link("created_by", "User")
     created_at = fields._DateTimeField("created_at")
     account_id = fields._UnEditableField("account_id")
