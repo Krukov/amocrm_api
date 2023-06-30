912069c99e330418b43cf98a3070bc9e396e7472
Evgeniy Semenov <edelwi@yandex.ru>
2023-06-30 18:04:32 +0300

    fixed bug https://github.com/Krukov/amocrm_api/issues/96

--- tests/data/companies.py
+++ tests/data/companies.py
@@ -21,3 +21,27 @@ DETAIL_INFO = {
     "_links": {"self": {"href": "https://exmaple.amocrm.ru/api/v4/companies/1"}},
     "_embedded": {"tags": []},
 }
+
+DETAIL_INFO_2 = {
+    "id": 1,
+    "name": "АО Копыта и рыла",
+    "responsible_user_id": 504141,
+    "group_id": 0,
+    "created_by": 504141,
+    "updated_by": 504141,
+    "created_at": 1582117331,
+    "updated_at": 1586361223,
+    "closest_task_at": None,
+    "custom_fields_values": [
+        {
+            "field_id": 3,
+            "field_name": "Телефон",
+            "field_code": "PHONE",
+            "field_type": "multitext",
+            "values": [{"value": "123213", "enum_id": 1, "enum": "WORK"}],
+        }
+    ],
+    "account_id": 10,
+    "_links": {"self": {"href": "https://exmaple.amocrm.ru/api/v4/companies/1"}},
+    "_embedded": {"tags": []},
+}
